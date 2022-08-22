ca7e45a76098b0d6048740db0bd87d08b8182098
skogseth <herman.skogseth@me.com>
2022-08-22 17:45:20 +0200

    Rewrote send and get modules with async using tokio. Test function for read/write updated

--- interface/src/get.rs
+++ interface/src/get.rs
@@ -1,79 +1,111 @@
-use std::io::prelude::*;
 use std::io::{Error, ErrorKind, Result};
-use std::net::TcpStream;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
 
 use crate::types::{Button, Floor};
 
-fn get_data(stream: &mut TcpStream, buffer: &mut [u8; 4]) -> Result<()> {
-    stream.write(buffer)?;
+async fn get_data(stream: &mut TcpStream, buffer: &mut [u8; 4]) -> Result<()> {
+    stream.write_all(buffer).await?;
     *buffer = [0; 4];
-    stream.read(buffer)?;
+    stream.read(buffer).await?;
     Ok(())
 }
 
-pub fn order_button(stream: &mut TcpStream, button: Button, floor: Floor) -> Result<bool> {
+pub async fn order_button(stream: &mut TcpStream, button: Button, floor: Floor) -> Result<bool> {
     let mut buffer: [u8; 4] = [6, u8::from(button), u8::from(floor), 0];
-    get_data(stream, &mut buffer)?;
+    get_data(stream, &mut buffer).await?;
 
     if buffer[0] != 6 {
-        return Err(Error::new(ErrorKind::InvalidData, "incorrect feedback for buffer[0] in floor()"));
+        return Err(Error::new(
+            ErrorKind::InvalidData,
+            "incorrect feedback for buffer[0] in floor()",
+        ));
     }
 
     let pressed = match buffer[1] {
         0 => false,
         1 => true,
-        _ => return Err(Error::new(ErrorKind::InvalidData, "incorrect feedback for buffer[1] in order_button()")),
+        _ => {
+            return Err(Error::new(
+                ErrorKind::InvalidData,
+                "incorrect feedback for buffer[1] in order_button()",
+            ))
+        }
     };
 
     Ok(pressed)
 }
 
-pub fn floor(stream: &mut TcpStream) -> Result<Option<Floor>> {
+pub async fn floor(stream: &mut TcpStream) -> Result<Option<Floor>> {
     let mut buffer: [u8; 4] = [7, 0, 0, 0];
-    get_data(stream, &mut buffer)?;
+    get_data(stream, &mut buffer).await?;
 
     if buffer[0] != 7 {
-        return Err(Error::new(ErrorKind::InvalidData, "incorrect feedback for buffer[0] in floor()"));
+        return Err(Error::new(
+            ErrorKind::InvalidData,
+            "incorrect feedback for buffer[0] in floor()",
+        ));
     }
 
     let floor = match buffer[1] {
         0 => None,
         1 => Floor::from_value(buffer[2] as usize),
-        _ => return Err(Error::new(ErrorKind::InvalidData, "incorrect feedback for buffer[1] in floor()")),
+        _ => {
+            return Err(Error::new(
+                ErrorKind::InvalidData,
+                "incorrect feedback for buffer[1] in floor()",
+            ))
+        }
     };
 
     Ok(floor)
 }
 
-pub fn stop(stream: &mut TcpStream) -> Result<bool> {
+pub async fn stop(stream: &mut TcpStream) -> Result<bool> {
     let mut buffer: [u8; 4] = [8, 0, 0, 0];
-    get_data(stream, &mut buffer)?;
+    get_data(stream, &mut buffer).await?;
 
     if buffer[0] != 8 {
-        return Err(Error::new(ErrorKind::InvalidData, "incorrect feedback for buffer[0] in stop()"));
+        return Err(Error::new(
+            ErrorKind::InvalidData,
+            "incorrect feedback for buffer[0] in stop()",
+        ));
     }
 
     let pressed = match buffer[1] {
         0 => false,
         1 => true,
-        _ => return Err(Error::new(ErrorKind::InvalidData, "incorrect feedback for buffer[1] in stop()")),
+        _ => {
+            return Err(Error::new(
+                ErrorKind::InvalidData,
+                "incorrect feedback for buffer[1] in stop()",
+            ))
+        }
     };
 
     Ok(pressed)
 }
 
-pub fn obstruction_switch(stream: &mut TcpStream) -> Result<bool> {
+pub async fn obstruction_switch(stream: &mut TcpStream) -> Result<bool> {
     let mut buffer: [u8; 4] = [9, 0, 0, 0];
-    get_data(stream, &mut buffer)?;
+    get_data(stream, &mut buffer).await?;
 
     if buffer[0] != 9 {
-        return Err(Error::new(ErrorKind::InvalidData, "incorrect feedback for buffer[0] in obstruction_switch()"));
+        return Err(Error::new(
+            ErrorKind::InvalidData,
+            "incorrect feedback for buffer[0] in obstruction_switch()",
+        ));
     }
 
     let active = match buffer[1] {
         0 => false,
         1 => true,
-        _ => return Err(Error::new(ErrorKind::InvalidData, "incorrect feedback for buffer[1] in stop()")),
+        _ => {
+            return Err(Error::new(
+                ErrorKind::InvalidData,
+                "incorrect feedback for buffer[1] in stop()",
+            ))
+        }
     };
 
     Ok(active)
--- interface/src/lib.rs
+++ interface/src/lib.rs
@@ -4,54 +4,56 @@ pub mod types;
 
 #[cfg(test)]
 mod tests {
-    use std::io::prelude::*;
     use std::net::SocketAddr;
-    use std::net::{TcpListener, TcpStream};
+    use tokio::io::{AsyncReadExt, AsyncWriteExt};
+    use tokio::net::{TcpListener, TcpStream};
 
-    #[test]
-    fn read_and_write() {
+    #[tokio::test]
+    async fn read_and_write() {
         const HOST: [u8; 4] = [127, 0, 0, 1];
         const PORT: u16 = 7878;
 
-        let handle = std::thread::spawn(|| {
+        let handle = tokio::spawn(async move {
             let addr = SocketAddr::from((HOST, PORT));
-            let listener = TcpListener::bind(addr).expect("failed to create listener");
-
-            let mut stream = match listener.accept() {
-                Ok((socket, _addr)) => socket,
-                Err(e) => panic!("failed to accept stream (listener) => error: {e}"),
+            let mut stream = match TcpStream::connect(addr).await {
+                Ok(s) => s,
+                Err(_) => panic!("failed to connect to addr {addr}"),
             };
 
-            let mut buffer: [u8; 5] = [0; 5];
-            if let Err(e) = stream.read(&mut buffer) {
-                panic!("failed to read buffer (listener) {buffer:?} => error: {e}");
+            let mut buffer: [u8; 5] = [1, 2, 3, 4, 5];
+            if let Err(e) = stream.write_all(&buffer).await {
+                panic!("failed to write buffer {buffer:?} => error: {e}");
             }
-            assert_eq!(buffer, [1, 2, 3, 4, 5]);
 
-            buffer = [5, 4, 3, 2, 1];
-            if let Err(e) = stream.write(&buffer) {
-                panic!("failed to write buffer (listener) {buffer:?} => error: {e}");
+            if let Err(e) = stream.read(&mut buffer).await {
+                panic!("failed to read to buffer {buffer:?} => error: {e}");
             }
+            assert_eq!(buffer, [5, 4, 3, 2, 1]);
         });
 
         let addr = SocketAddr::from((HOST, PORT));
-        let mut stream = match TcpStream::connect(addr) {
-            Ok(s) => s,
-            Err(_) => panic!("failed to connect to addr {addr}"),
+        let listener = TcpListener::bind(addr)
+            .await
+            .expect("failed to create listener");
+
+        let mut stream = match listener.accept().await {
+            Ok((socket, _addr)) => socket,
+            Err(e) => panic!("failed to accept stream (listener) => error: {e}"),
         };
 
-        let mut buffer: [u8; 5] = [1, 2, 3, 4, 5];
-        if let Err(e) = stream.write(&buffer) {
-            panic!("failed to write buffer {buffer:?} => error: {e}");
+        let mut buffer: [u8; 5] = [0; 5];
+        if let Err(e) = stream.read(&mut buffer).await {
+            panic!("failed to read buffer (listener) {buffer:?} => error: {e}");
+        }
+        assert_eq!(buffer, [1, 2, 3, 4, 5]);
+
+        buffer = [5, 4, 3, 2, 1];
+        if let Err(e) = stream.write_all(&buffer).await {
+            panic!("failed to write buffer (listener) {buffer:?} => error: {e}");
         }
 
-        if let Err(e) = stream.read(&mut buffer) {
-            panic!("failed to read to buffer {buffer:?} => error: {e}");
-        }
-        assert_eq!(buffer, [5, 4, 3, 2, 1]);
-
-        if let Err(_) = handle.join() {
-            panic!("failed to join listener thread");
+        if let Err(e) = handle.await {
+            panic!("failed to await handle => error: {e}");
         }
     }
 }
